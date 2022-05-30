996460c72c527b83b4949c2174d9bd9277e9a679
tomasgenco <102707915+tomasgenco@users.noreply.github.com>
2022-05-30 20:31:26 -0300

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,41 @@
+// JAVA
+// let entrada = prompt("SELECCIONAR PRODUCTO DE 1 A 4")
+
+// while(entrada != "ESC"){
+//     // si se quiere salir del bucle escribir "ESC"
+//     switch (entrada){
+//         case "1":
+//             alert("Tomate")
+//             break
+//         case "2":
+//             alert("Papa")
+//             break
+//         case "3":
+//             alert("Carne")
+//             break
+//         case "4":
+//             alert("Pollo")
+//             break
+//     }
+//     entrada = prompt("SELECCIONAR PRODUCTO DE 1 A 4"){
+//         break
+//     }
+    
+// }
+
+let y = parseInt(prompt("Ingrese un numero"))
+let texto = (prompt("Ingrese un texto"))
+
+for (let i = 0;i < y; i++){
+    if(y == 666){
+        alert("NUMERO PROHIBIDO")
+        break
+    }
+    console.log(texto + i)
+}
+
+
+
+
+
+
